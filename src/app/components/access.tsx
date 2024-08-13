92d3099cf6a40b1fa4839f0b1467b865f2ece970
Jastin-Infj <takumiyokoo@gmail.com>
2024-08-13 18:49:18 +0900

    [change] database init before

--- src/app/components/access.tsx
+++ src/app/components/access.tsx
@@ -4,10 +4,16 @@ import { PokemonClient  , MoveClient } from 'pokenode-ts';
 import { PokemonAPIObject, PokemonDataBase } from '@/types';
 
 import fs from 'fs';
+import React from "react";
 
 const JSON_POKEMON_DEX_PATH = "./src/json/pokemonDex.json";
 const JSON_POKEMON_INFO_PATH = "./src/json/pokemonInfo.json";
 const JSON_POKEMON_SPEC_PATH = "./src/json/pokemonSpec.json";
+const JSON_POKEMON_TYPE_PATH = "./src/json/pokemonType.json";
+const JSON_POKEMON_MOVE_PATH = "./src/json/pokemonMove.json"; 
+const JSON_POKEMON_ABILITY_PATH = "./src/json/pokemonAbility.json";
+const JSON_POKEMON_ITEM_PATH = "./src/json/pokemonItem.json";
+const JSON_POKEMON_NATURE_PATH = "./src/json/pokemonNature.json";
 
 let DEBUG_FLAG = false;
 
@@ -32,10 +38,18 @@ export const Access = () => {
     moves: []
   };
 
+  let allDexInfo: PokemonAPIObject[] = [];
+  let allPokemonInfo: Object[] = [];
+  let allSpecInfo: Object[] = [];
+  let allTypesInfo: PokemonAPIObject[] = [];
+  let allMoveInfo: PokemonAPIObject[] = [];
+  let allAbilityInfo: PokemonAPIObject[] = [];
+  let allItemInfo: PokemonAPIObject[] = [];
+  let allNatureInfo: PokemonAPIObject[] = [];
+
   const handlePokemon = async (): Promise<PokemonAPIObject[]> => {
 
     let allPokemon: PokemonAPIObject[] = [];
-
     // すでにファイルが存在する場合はそれを読み込む
     if(fs.existsSync(JSON_POKEMON_DEX_PATH)){
       const fileContent = fs.readFileSync(JSON_POKEMON_DEX_PATH , 'utf-8');
@@ -54,7 +68,6 @@ export const Access = () => {
       allPokemon = allPokemon.concat(pokemonList.results);
       offset += LIMIT;
       if (pokemonList.results.length < LIMIT) {
-        fs.writeFileSync(JSON_POKEMON_DEX_PATH, JSON.stringify(allPokemon, null, 2));
         return allPokemon;
       }
     }
@@ -157,34 +170,41 @@ export const Access = () => {
     return posts;
   }
 
-  // const posts = getPosts();
-  // posts.then((data) => {
-  //   console.log(data[1]);
-  // });
 
   // 外部キー制約により順番を決める
   const InsertDexInfo = async () => {
     const api = new PokemonClient();
+    
+    console.log("             ");
+    console.log("--- Start ---");
 
-    let DexInfo  = await handlePokemon();
+    // ポケモン図鑑情報を取得
+    if(fs.existsSync(JSON_POKEMON_DEX_PATH)) {
+      const fileContent = fs.readFileSync(JSON_POKEMON_DEX_PATH , 'utf-8');
+      const json: PokemonAPIObject[] = JSON.parse(fileContent);
+      allDexInfo = json;
+    } else {
+      const dexInfo = await handlePokemon();
+      fs.writeFileSync(JSON_POKEMON_DEX_PATH, JSON.stringify(dexInfo, null, 2));
+      allDexInfo = dexInfo;
+    }
     console.log("--- DexInfo Fin ---");
 
-    // getParam: id 
-    let PokemonList:Object[] = [];
 
     // すでにファイルが存在する場合はjson読み込む
     if(fs.existsSync(JSON_POKEMON_INFO_PATH)){
       const fileContent = fs.readFileSync(JSON_POKEMON_INFO_PATH , 'utf-8');
       const json: Object[] = JSON.parse(fileContent);
-      PokemonList = json;
+      allPokemonInfo = json;
     } else {
-      PokemonList = await Promise.all(
-        DexInfo.map(async (data) => {
+      const PokemonList = await Promise.all(
+        allDexInfo.map(async (data) => {
           const res = await fetch(data.url);
           return res.json();
         })
       );
       fs.writeFileSync(JSON_POKEMON_INFO_PATH, JSON.stringify(PokemonList, null, 2));
+      allPokemonInfo = PokemonList;
     }
     console.log("--- PokemonList Fin ---");
 
@@ -194,10 +214,10 @@ export const Access = () => {
     if(fs.existsSync(JSON_POKEMON_SPEC_PATH)){
       const fileContent = fs.readFileSync(JSON_POKEMON_SPEC_PATH , 'utf-8');
       const json: Object[] = JSON.parse(fileContent);
-      SpecInfo = json;
+      allSpecInfo = json;
     } else {
-      SpecInfo = await Promise.all(
-        PokemonList.map(async (data:any) => {
+      const SpecInfo = await Promise.all(
+        allPokemonInfo.map(async (data:any) => {
           const url = `https://pokeapi.co/api/v2/pokemon-species/${data.id}/`;
   
           try {
@@ -214,13 +234,14 @@ export const Access = () => {
       );
       // 非同期処理完了後にファイルに書き込む
       fs.writeFileSync(JSON_POKEMON_SPEC_PATH, JSON.stringify(SpecInfo, null, 2));
+      allSpecInfo = SpecInfo;
     }
-    console.log("--- SpecInfo ---");
+    console.log("--- SpecInfo Fin ---");
     
     // 日本語の名前を取得
-    SpecInfo.forEach((data:any) => {
+    allSpecInfo.forEach((data:any) => {
       if(data.names === null) {
-        console.log([data.id, "" , data.name]);
+        // console.log([data.id, "" , data.name]);
         return;
       }
 
@@ -236,99 +257,81 @@ export const Access = () => {
             break;
         }
       });
-      console.log([data.id , Param_Name_JA , Param_Name_EN]);
+      // console.log([data.id , Param_Name_JA , Param_Name_EN]);
     });
 
-    // let TypeInfo = handleTypes();
-    // let MoveInfo = handleMoves();
-    // let AbilityInfo = handleAbility();
-    // let ItemInfo = handleItem();
-    // let NatureInfo = handleNature();
+    // タイプ情報を取得
+    if(fs.existsSync(JSON_POKEMON_TYPE_PATH)) {
+      const fileContent = fs.readFileSync(JSON_POKEMON_TYPE_PATH, 'utf-8');
+      const json: PokemonAPIObject[] = JSON.parse(fileContent);
+      allTypesInfo = json;
+    } else {
+      const TypeList = await handleTypes();
+      fs.writeFileSync(JSON_POKEMON_TYPE_PATH, JSON.stringify(TypeList, null, 2));
+      allTypesInfo = TypeList;
+    }
+    console.log("--- TypeList Fin ---");
 
-    // 全ての処理が終わるまで待つ
-    // Promise.all([DexInfo]).then((results) => {
-      
-    //   // DexInfo のURLを参照する
-    //   let dex = results[0];
+    // 技情報を取得
+    if(fs.existsSync(JSON_POKEMON_MOVE_PATH)) {
+      const fileContent = fs.readFileSync(JSON_POKEMON_MOVE_PATH, 'utf-8');
+      const json: PokemonAPIObject[] = JSON.parse(fileContent);
+      allMoveInfo = json;
+    } else {
+      const MoveList = await handleMoves();
+      fs.writeFileSync(JSON_POKEMON_MOVE_PATH, JSON.stringify(MoveList, null, 2));
+      allMoveInfo = MoveList;
+    }
+    console.log("--- MoveList Fin ---");
 
-    //   // それぞれの情報を取得する
-    //   let Param_Name_JA = "";
-    //   let Param_Name_EN = "";
+    // アビリティ情報を取得
+    if(fs.existsSync(JSON_POKEMON_ABILITY_PATH)) {
+      const fileContent = fs.readFileSync(JSON_POKEMON_ABILITY_PATH, 'utf-8');
+      const json: PokemonAPIObject[] = JSON.parse(fileContent);
+      allAbilityInfo = json;
+    } else {
+      const AbilityList = await handleAbility();
+      fs.writeFileSync(JSON_POKEMON_ABILITY_PATH, JSON.stringify(AbilityList, null, 2));
+      allAbilityInfo = AbilityList;
+    }
+    console.log("--- AbilityList Fin ---");
 
-    //   let Pokemoninfo;
-    //   // すべての非同期処理を待機
-    //   if(JSON_POKEMON_INFO !== undefined) {
-    //     Pokemoninfo = Promise.resolve(JSON_POKEMON_INFO);
-    //   } else {
-    //     Pokemoninfo = Promise.all(dex.map(async (data) => {
-    //       const res = await fetch(data.url);
-    //       const json = await res.json();
-    //       return Promise.resolve(json);
-    //     }));
-    //   }
-      
-    //   // 日本語の名前を取得
-    //   return Pokemoninfo.then((datas:any) => {
-    //     console.log("Pokemoninfo Fin");
-    //     console.log(typeof datas);
+    // アイテム情報を取得
+    if(fs.existsSync(JSON_POKEMON_ITEM_PATH)) {
+      const fileContent = fs.readFileSync(JSON_POKEMON_ITEM_PATH , 'utf-8');
+      const json: PokemonAPIObject[] = JSON.parse(fileContent);
+      allItemInfo = json;
+    } else {
+      const ItemList = await handleItem();
+      fs.writeFileSync(JSON_POKEMON_ITEM_PATH, JSON.stringify(ItemList, null, 2));
+      allItemInfo = ItemList;
+    }
+    console.log("--- ItemList Fin ---");
 
-    //     if(JSON_POKEMON_INFO === undefined) {
-    //       fs.writeFileSync(JSON_POKEMON_INFO_PATH, JSON.stringify(datas, null, 2));
-    //     }
+    // 性格情報を取得
+    let NatureList: PokemonAPIObject[] = [];
+    if(fs.existsSync(JSON_POKEMON_NATURE_PATH)) {
+      const fileContent = fs.readFileSync(JSON_POKEMON_NATURE_PATH , 'utf-8');
+      const json: PokemonAPIObject[] = JSON.parse(fileContent);
+      allNatureInfo = json;
+    } else {
+      const NatureList = await handleNature();
+      fs.writeFileSync(JSON_POKEMON_NATURE_PATH, JSON.stringify(NatureList, null, 2));
+      allNatureInfo = NatureList;
+    }
+    console.log("--- NatureList Fin ---");
 
-    //     // 情報源を取得
-    //     let SpecInfo = Promise.all(datas.map(async (data:any) => {
-    //       console.log("Check : " + data.id);
+    /// データベースに挿入
+    console.log("             ");
+    console.log("--- Data Insert Start ---");
 
-    //       const url = `https://pokeapi.co/api/v2/pokemon-species/${data.id}/`;
-    //       const res = await fetch(url);
-    //       // 404エラーが出た場合はnullを返す
-    //       if(res.status === 404) return { id: data.id , names: null};
-
-    //       const json:any = await res.json();
-    //       return Promise.resolve(json);
-    //     })).then((res) => {
-          
-    //       let test = res.map(async (data) => {
-
-    //         if(data.names === null) {
-    //           //console.log([data.id, "" , data.nameEn]);
-    //           return Promise.resolve(data);
-    //         }
-
-    //         // 日本語名と英語名を取得
-    //         data.names.map(async (nameParam:any) => {
-    //           switch (nameParam.language.name) {
-    //             case "ja":
-    //               Param_Name_JA = nameParam.name;
-    //               break;
-    //             case "en":
-    //               Param_Name_EN = nameParam.name;
-    //               break;
-    //           }
-    //         });
-
-    //         //console.log([data.id , Param_Name_JA , Param_Name_EN]);
-
-    //         const json = {
-    //           id: data.id,
-    //           nameJa: Param_Name_JA,
-    //           nameEn: Param_Name_EN,
-    //         };
-    //         return Promise.resolve(json);
-    //       });
-
-    //       Promise.all([test]).then((res) => {
-    //         // console.log(res);
-    //       });
-
-    //     });
-    //   });
-
-    // });
   }
 
   InsertDexInfo();
+  // const posts = getPosts();
+  // posts.then((data) => {
+  //   console.log(data[1]);
+  // });
 
   return (
     <button>test</button>
