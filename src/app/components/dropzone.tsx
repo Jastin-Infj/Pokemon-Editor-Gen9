9e7df4b00067688791c935329f738cff9ff92abe
Jastin-Infj <takumiyokoo@gmail.com>
2024-09-22 20:48:24 +0900

    [add] file multi import load frontend

--- src/app/components/dropzone.tsx
+++ src/app/components/dropzone.tsx
@@ -11,146 +11,151 @@ interface Props {
 
 const MyDropzone:React.FC<Props> = ({dispatch_P_datas , P_datas}) => {
   const [hexStrings , setHexStrings ] = useState<string[]>([]);
-  const [pokemonData , setPokemonData] = useState<RequestPokemonData | null>(null);
+  const [requestpokemonData , setrequestPokemonData] = useState<RequestPokemonData[]>([]);
   const [fetchData , setFetchData] = useState<any>(null);
-
-  // TODO 複数ファイルの対応
-
+  
   // pokemonData が変更されたら、fetchData を実行
   useEffect(() => {
-    if(!pokemonData) return;
+    if(requestpokemonData.length === 0) return;
+
     console.log("useEffect called");
     const fetchData = async () => {
-      // 内部データを参考に取得
-      const res:any = await reducer_RequestPokemonData({type: "FILE", payload: pokemonData});
-      setFetchData(res);
-      console.log(res);
+      const newFetchDataList = await Promise.all(requestpokemonData.map(async (data) => {
+        const res:any = await reducer_RequestPokemonData({type: "FILE", payload: data});
+        return res;
+      }));
+      setFetchData(newFetchDataList);
+      console.log(newFetchDataList);
 
-      let newPBase = Create_PBaseProps("FETCH", res);
-      dispatch_P_datas({type: "ADD", payload: newPBase});
+      newFetchDataList.forEach((res) => {
+        let newPBase = Create_PBaseProps("FETCH", res);        // 追加
+        dispatch_P_datas({ type: "ADD", payload: newPBase });
+      });
+      // 追加データをリセットする
+      setrequestPokemonData([]);
+
       setFetchData(null);
       console.log("useEffect fin");
     };
+
     fetchData();
-  },[pokemonData]);
+  },[requestpokemonData]);
 
   const onDrop = (acceptedFiles: File[]) => {
     console.log("onDrop called");
-
-    const file = acceptedFiles[0];
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      console.log("File Reader onload called");
-      const binaryString = event.target?.result as ArrayBuffer;
-      if (binaryString) {
-         const newHexString = Array.from(new Uint8Array(binaryString)).map(byte => byte.toString(16).padStart(2, '0')).join('');
-        setHexStrings([...hexStrings, newHexString]);
-
-        let f_id:number;
-        let f_move1:number;
-        let f_move2:number;
-        let f_move3:number;
-        let f_move4:number;
-        let f_ability:number;
-        let f_item:number;
-        let f_natureCurrent:number;
-        let f_teraTypeCurrent:number;
-        
-        {
-          // Pokemon
-          const pokemon = newHexString.substring(16 , 16 + 4);
-          // バイトオーダーを逆にする
-          const pokemonDec = pokemon.substring(2, 4) + pokemon.substring(0, 2);
-          // 16進数を10進数に変換
-          const decimalValue = parseInt(pokemonDec, 16);
-          console.log(`Pokemon: ${decimalValue}`);
-          f_id = decimalValue;
-        }
-
-        {
-          // Item
-          const item = newHexString.substring(20 , 20 + 4);
-          // バイトオーダーを逆にする
-          const itemDec = item.substring(2, 4) + item.substring(0, 2);
-          // 16進数を10進数に変換
-          const decimalValue = parseInt(itemDec, 16);
-          console.log(`Item: ${decimalValue}`);
-          f_item = decimalValue;
-        }
-
-        // ability
-        const abilityList: number[] = [];
-        {
-          const ability = newHexString.substring(40 , 40 + 4);
-          // バイトオーダーを逆にする
-          const abilityDec = ability.substring(2, 4) + ability.substring(0, 2);
-          // 16進数を10進数に変換
-          const decimalValue = parseInt(abilityDec, 16);
-          abilityList.push(decimalValue);
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const binaryString = event.target?.result as ArrayBuffer;
+        if (binaryString) {
+          const newHexString = Array.from(new Uint8Array(binaryString)).map(byte => byte.toString(16).padStart(2, '0')).join('');
+          setHexStrings([...hexStrings, newHexString]);
   
-          // 何番目の特性かどうか？
-          const abilityNum = newHexString.substring(44 , 44 + 2);
-          const decimalValueNum = parseInt(abilityNum, 16);
-          // 1: SET1  2: SET2  4: SET3
-          abilityList.push(decimalValueNum);
-        }
-        console.log(`Ability: ${abilityList}`);
-        f_ability = abilityList[0];
-
-        // nature
-        const natureList: number[] = [];
-        for(let i = 0; i < 2; i++) {
-          const nature = newHexString.substring(64 + (i * 2) ,64 + (i * 2) + 2);
-          const decimalValue = parseInt(nature , 16);
-          natureList.push(decimalValue);
-        }
-        console.log(`Nature: ${natureList}`);
-        f_natureCurrent = natureList[1];
-
-        // move
-        const list: number[] = [];
-        for (let i = 0; i < 4; i++) {
-          const move = newHexString.substring(228 + (i * 4), 228 + (i * 4) + 4);
-          const reversedMove = move.substring(2, 4) + move.substring(0, 2); // バイトオーダーを逆にする
-          const decimalValue = parseInt(reversedMove, 16); // 16進数を10進数に変換
-          list.push(decimalValue);
-        }
-        console.log(`Move: ${list}`);
-        f_move1 = list[0];
-        f_move2 = list[1];
-        f_move3 = list[2];
-        f_move4 = list[3];
-
-        // teratype
-        const teratypes: number[] = [];
-        {
-          for (let i = 0; i < 2; i++) {
-            const tetatype = newHexString.substring(296 + (i * 2), 296 + (i * 2) + 2);
-            const decimalValue = parseInt(tetatype, 16); // 16進数を10進数に変換
-            teratypes.push(decimalValue);
+          let f_id:number;
+          let f_move1:number;
+          let f_move2:number;
+          let f_move3:number;
+          let f_move4:number;
+          let f_ability:number;
+          let f_item:number;
+          let f_natureCurrent:number;
+          let f_teraTypeCurrent:number;
+          
+          {
+            // Pokemon
+            const pokemon = newHexString.substring(16 , 16 + 4);
+            // バイトオーダーを逆にする
+            const pokemonDec = pokemon.substring(2, 4) + pokemon.substring(0, 2);
+            // 16進数を10進数に変換
+            const decimalValue = parseInt(pokemonDec, 16);
+            console.log(`Pokemon: ${decimalValue}`);
+            f_id = decimalValue;
           }
-        }
-        // 99: stera  Base , Edit
-        console.log(`Teratype Base: ${teratypes[0]} , Edit: ${teratypes[1]}`);
-        f_teraTypeCurrent = teratypes[1];
-
-        const request: RequestPokemonData = {
-          nationalAPI: f_id,
-          move1: f_move1,
-          move2: f_move2,
-          move3: f_move3,
-          move4: f_move4,
-          ability: f_ability,
-          item: f_item,
-          natureCurrent: f_natureCurrent,
-          teraTypeCurrent: f_teraTypeCurrent,
-        };
-        setPokemonData(request);
-
-      } // if (binaryString)
-    }
-    reader.readAsArrayBuffer(file);
-  }
+  
+          {
+            // Item
+            const item = newHexString.substring(20 , 20 + 4);
+            // バイトオーダーを逆にする
+            const itemDec = item.substring(2, 4) + item.substring(0, 2);
+            // 16進数を10進数に変換
+            const decimalValue = parseInt(itemDec, 16);
+            console.log(`Item: ${decimalValue}`);
+            f_item = decimalValue;
+          }
+  
+          // ability
+          const abilityList: number[] = [];
+          {
+            const ability = newHexString.substring(40 , 40 + 4);
+            // バイトオーダーを逆にする
+            const abilityDec = ability.substring(2, 4) + ability.substring(0, 2);
+            // 16進数を10進数に変換
+            const decimalValue = parseInt(abilityDec, 16);
+            abilityList.push(decimalValue);
+    
+            // 何番目の特性かどうか？
+            const abilityNum = newHexString.substring(44 , 44 + 2);
+            const decimalValueNum = parseInt(abilityNum, 16);
+            // 1: SET1  2: SET2  4: SET3
+            abilityList.push(decimalValueNum);
+          }
+          console.log(`Ability: ${abilityList}`);
+          f_ability = abilityList[0];
+  
+          // nature
+          const natureList: number[] = [];
+          for(let i = 0; i < 2; i++) {
+            const nature = newHexString.substring(64 + (i * 2) ,64 + (i * 2) + 2);
+            const decimalValue = parseInt(nature , 16);
+            natureList.push(decimalValue);
+          }
+          console.log(`Nature: ${natureList}`);
+          f_natureCurrent = natureList[1];
+  
+          // move
+          const list: number[] = [];
+          for (let i = 0; i < 4; i++) {
+            const move = newHexString.substring(228 + (i * 4), 228 + (i * 4) + 4);
+            const reversedMove = move.substring(2, 4) + move.substring(0, 2); // バイトオーダーを逆にする
+            const decimalValue = parseInt(reversedMove, 16); // 16進数を10進数に変換
+            list.push(decimalValue);
+          }
+          console.log(`Move: ${list}`);
+          f_move1 = list[0];
+          f_move2 = list[1];
+          f_move3 = list[2];
+          f_move4 = list[3];
+  
+          // teratype
+          const teratypes: number[] = [];
+          {
+            for (let i = 0; i < 2; i++) {
+              const tetatype = newHexString.substring(296 + (i * 2), 296 + (i * 2) + 2);
+              const decimalValue = parseInt(tetatype, 16); // 16進数を10進数に変換
+              teratypes.push(decimalValue);
+            }
+          }
+          // 99: stera  Base , Edit
+          console.log(`Teratype Base: ${teratypes[0]} , Edit: ${teratypes[1]}`);
+          f_teraTypeCurrent = teratypes[1];
+  
+          const request: RequestPokemonData = {
+            nationalAPI: f_id,
+            move1: f_move1,
+            move2: f_move2,
+            move3: f_move3,
+            move4: f_move4,
+            ability: f_ability,
+            item: f_item,
+            natureCurrent: f_natureCurrent,
+            teraTypeCurrent: f_teraTypeCurrent,
+          };
+          setrequestPokemonData((prevlist) => [...prevlist , request]);
+        } // if (binaryString)
+      } // onload
+      reader.readAsArrayBuffer(file);
+    });
+  };
 
   const {getRootProps , getInputProps } = useDropzone({onDrop});
 
@@ -161,8 +166,8 @@ const MyDropzone:React.FC<Props> = ({dispatch_P_datas , P_datas}) => {
         <p className='text-center translate-y-1/2'>ファイルをここにドロップするか、クリックして選択してください</p>
       </div>
       <ul>
-        {pokemonData && Object.entries(pokemonData).map(([key, value]) => (
-          <li key={key}>{key}: {value}</li>
+        {requestpokemonData && Object.entries(requestpokemonData).map(([key, value]) => (
+          <li key={key}>{key}: {}</li>
         ))}
       </ul>
       {hexStrings.map((hexString, index) => (
